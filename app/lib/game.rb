a11071018446c69f9c0fff0fcaf94ae903f7130e
AKhan-CIR <akhan@codeinreach.com>
2019-08-01 15:04:11 -0700

    Changed names of functions, fixed quotes

--- app/lib/game.rb
+++ app/lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-    def walk_into_a_bar
+    def walk_into_a_bar #INTRO
         puts "A familiar face at the bar greets you. \"Hey, long time no see! What was your name again?\""
         $name = STDIN.gets.chomp.capitalize
         user = User.find_by(username: $name)
@@ -10,7 +10,7 @@ class Game
             user = User.create(username: $name)
         end
 
-        puts "You look around the room and make eye contact with a hottie. Your friend notices and says, \"You should go talk to that hottie over there.\""
+        puts "You look around the room and make eye contact with someone. Your friend notices and says, \"You should go talk to that hottie over there.\""
 
         npc = Npc.all.sample 
         puts "You see #{npc.name}"   
@@ -34,8 +34,8 @@ class Game
 
         go_over(rel)
     end
-
-    def go_over(rel)
+    
+    def go_over(rel)#STEP1
         
         puts "1) Go over and talk to #{rel.npc.name} 2) Run away."
         action = STDIN.gets.chomp
@@ -43,21 +43,23 @@ class Game
             puts "You start walking in #{rel.npc.name}'s direction when someone approaches them first. What do you do?"
             enter_conversation(rel)        
         elsif action == "2"
+            puts "\"Fortune favors the bold\"-Virgil"
+            
             puts "GAME OVER!" 
-            puts "Fortune favors the bold. -Virgil"
+            walk_into_a_bar(rel)
         else 
             puts "Just be yourself."
             puts "Pick again"
             go_over(rel)
         end
     end
-
-    def enter_conversation(rel)
+    
+    def enter_conversation(rel)#STEP2
         puts "1) Enter their conversation. 2) Sucker punch competition in the face."
         action = STDIN.gets.chomp
         if action == "1"
             puts "You say \"Looks like the party is over here!\""
-            puts "The #{rel.npc.name} laughs and smiles. The competition frowns."
+            puts "#{rel.npc.name} laughs and smiles. The competition frowns."
             rel.points += 7
             rel.save    
             puts "You have #{rel.points} points with #{rel.npc.name}"
@@ -77,7 +79,7 @@ class Game
         end
     end
 
-    def conversate(rel)
+    def conversate(rel)#STEP3
         puts "1) Use pickup line. 2) Insult."
         action = STDIN.gets.chomp
         if action == "1"
@@ -85,8 +87,8 @@ class Game
             puts "#{rel.npc.name} is delighted. In the face of superior game, the competition surrenders and retreats."
             introductions(rel)
         elsif action == "2"
+            puts "\"An injury is much sooner forgotten than an insult.\"-Philip Stanhope, 4th Earl of Chesterfield"
             puts "GAME OVER!"
-            puts "An injury is much sooner forgotten than an insult. -Philip Stanhope, 4th Earl of Chesterfield"
             walk_into_a_bar(rel)
         else 
             puts "Just be yourself."
@@ -95,143 +97,143 @@ class Game
         end
     end
 
-    def introductions(rel)
+    def introductions(rel)#STEP4
         puts "1) Introduce yourself. 2) Ask for phone number."
         action = STDIN.gets.chomp
         if action == "1"
             puts "You say \"Hey I'm #{$name.capitalize}\""
             puts "#{rel.npc.name}  says \"Hey I'm #{rel.npc.name} . It's hot in here...\""
-            step5
+            buy_you_a_drank(rel)
         elsif action == "2"
+            puts "\"Don't talk to strangers.\"-Mom & Dad"
             puts "GAME OVER!"
-            puts "\"Don't talk to strangers.\" -Mom & Dad"
-            intro
+            walk_into_a_bar(rel)
         else 
             puts "Just be yourself."
             puts "Pick again"
-            step4
+            introductions(rel)
         end
     end
 
-    def step5
+    def buy_you_a_drank(rel)#STEP5
         
         puts "1) Offer to buy drink. 2) Shrug shoulders."
         action = STDIN.gets.chomp
         if action == "1"
             puts "You say \"Can I buy you a drink?\""
-            puts "The hottie says \"Sure!\""
+            puts "#{rel.npc.name} says \"Sure!\""
             puts "The bartender is busy in the crowded bar. He skips your turn and gives his attention to someone else."
-            step6
+            the_key_to_all_virtues(rel)
         elsif action == "2"
+            puts "\"The heart that gives, gathers.\"-Tao Te Ching"
             puts "GAME OVER!"
-            puts "“The heart that gives, gathers.” Tao Te Ching"
-            intro
+            walk_into_a_bar(rel)
         else 
             puts "Just be yourself."
             puts "Pick again"
-            step5
+            buy_you_a_drank(rel)
         end
     end
 
-    def step6
+    def the_key_to_all_virtues(rel)
         puts "1) Stay cool. 2) Get angry."
         action = STDIN.gets.chomp
         if action == "1"
             puts "The bartender asks what the two of you would like to drink."
-            puts "The hottie tells, \"I'll have a mimosa.\""
-            step7
+            puts "The #{rel.npc.name} says, \"I'll have a mimosa.\""
+            get_courage(rel)
         elsif action == "2"
+            puts "#{rel.npc.name} gets embarassed by you and leaves."
+            puts "\"Patience is not simply the ability to wait - it's how we behave while we're waiting.\"-Joyce Meyer"
             puts "GAME OVER!"
-            puts "The hottie gets embarassed by you and leaves."
-            puts "Patience is not simply the ability to wait - it's how we behave while we're waiting. Joyce Meyer"
-            intro
+            walk_into_a_bar(rel)
         else 
             puts "Just be yourself."
             puts "Pick again"
-            step6
+            the_key_to_all_virtues(rel)
         end
     end
 
-    def step7
+    def get_courage(rel)
         puts "1) Order two mimosas. 2) Order two AMFs."
         action = STDIN.gets.chomp
         if action == "1"
             puts "The bartender serves two mimosas."
-            puts "The hottie says, \"Thanks!\""
-            puts "The DJ starts playing \"Back That Azz Up by Juvenile\". The hottie says \"This is my favorite song!\""
-            step8
+            puts "#{rel.npc.name} says, \"Thanks!\""
+            puts "The DJ starts playing \"Back That Azz Up by Juvenile\". #{rel.npc.name} says \"This is my favorite song!\""
+            get_funky(rel)
         elsif action == "2"
+            puts "The #{rel.npc.name} takes the AMF and dumps it on your head."
+            puts "\"The simple act of paying attention can take you a long way.\"-Keanu Reeves"
             puts "GAME OVER!"
-            puts "The hottie takes the AMF and dumps it on your head."
-            puts "The simple act of paying attention can take you a long way. Keanu Reeves"
-            intro
+            walk_into_a_bar(rel)
         else 
             puts "Just be yourself."
             puts "Pick again"
-            step7
+            get_courage(rel)
         end
     end
 
-    def step8
-        puts "Hottie wants to dance but you're a terrible dancer."
-        puts "1) Excuse yourself to go to the restroom. 2) Lead hottie to dance floor."
+    def get_funky(rel)
+        puts "#{rel.npc.name} wants to dance but you're a terrible dancer."
+        puts "1) Excuse yourself to go to the restroom. 2) Lead #{rel.npc.name} to dance floor."
         action = STDIN.gets.chomp
         if action == "1"
             puts "GAME OVER!"
-            puts "You go to the restroom and there is a huge line. When you return to the dance floor the hottie is dancing with someone else."
-            puts "Bravery is being the only one who knows you're afraid. Franklin P. Jones  "   
-            intro
+            puts "You go to the restroom and there is a huge line. When you return to the dance floor #{rel.npc.name} is dancing with someone else."
+            puts "\"Bravery is being the only one who knows you're afraid.\"-Franklin P. Jones"   
+            walk_into_a_bar(rel)
         elsif action == "2"
             puts "The space between your bodies leaves no room for the Holy Spirit."
-            puts "The hottie gazes into your eyes."
-            step9
+            puts "The #{rel.npc.name} gazes into your eyes."
+            like_nike(rel)
         else 
             puts "Just be yourself."
             puts "Pick again"
-            step8
+            get_funky(rel)
         end
     end
 
-    def step9
+    def like_nike(rel)
         puts "1) Go for the kiss. 2) Hesitate."
         action = STDIN.gets.chomp
         if action == "1"
             puts "Your eyes close. Your lips meet. Everything else in the moment disappears."
-            step10  
+            last_call(rel)
         elsif action == "2"
             puts "GAME OVER!"
-            puts "The hottie looks away and says \"I have to go. Bye.\""
-            puts "People, y'know, they either love us or they hate us; there's no middle ground. Ace Frehley"
-            intro
+            puts "#{rel.npc.name} looks away and says \"I have to go. Bye.\""
+            puts "\"People, y'know, they either love us or they hate us; there's no middle ground.\"-Ace Frehley"
+            walk_into_a_bar(rel)
         else 
             puts "\"Just be yourself.\""
             puts "Pick again"
-            step9
+            like(rel)
         end
     end
 
-    def step10
+    def last_call(rel)
         puts "The lights turn on. The music stops. Friend of the hottie says \"Our Uber is here.\""
-        puts "1) Grab hottie's arm and be possessive. 2) Ask for phone number."
+        puts "1) Grab #{rel.npc.name}'s arm and be possessive. 2) Ask for phone number."
         action = STDIN.gets.chomp
         if action == "1"
-            puts "Hottie's friend punches you. The bouncers grab you and throw you into the dumpster in the alleyway."
-            puts "If you love something let it go. If it comes back it's yours, and that's how you know. Christina Aguilera"
+            puts "#{rel.npc.name}'s friend punches you. The bouncers grab you and throw you into the dumpster in the alleyway."
+            puts "\"If you love something let it go. If it comes back it's yours, and that's how you know.\"-Christina Aguilera"
             puts "GAME OVER!"
         elsif action == "2"
             puts "VICTORY!"
-            puts "You trade phone numbers with the hottie. The hottie gets into the Uber and leaves."
+            puts "You trade phone numbers with #{rel.npc.name}. #{rel.npc.name} gets into the Uber and leaves."
             puts "Play again?"
                 input = STDIN.gets.chomp
                 if input == "yes" || input ==  "y"
-                    intro
+                    walk_into_a_bar(rel)
                 elsif input == "no" || input ==  "n"
                     puts "THE END"
                 end   
         else 
             puts "Just be yourself."
             puts "Pick again"
-            step10
+            last_call(rel)
         end
     end
 end
